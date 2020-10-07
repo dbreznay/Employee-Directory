d35859173c757202c888ad0fb4827a8f9fe17a9a
dbreznay <60904436+dbreznay@users.noreply.github.com>
2020-10-06 22:17:59 -0500

    deploy to gh-pages

---
+++ build/precache-manifest.bbc5c24857f954fe3f604c9a26c0db25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7aee02510522c7a8a3e9bdde609b29e",
+    "url": "/dbreznay/employee-directory/index.html"
+  },
+  {
+    "revision": "85be6c803b2a4012dda4",
+    "url": "/dbreznay/employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a99a5eea9dfa815a85fc",
+    "url": "/dbreznay/employee-directory/static/js/2.8e1b47c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dbreznay/employee-directory/static/js/2.8e1b47c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85be6c803b2a4012dda4",
+    "url": "/dbreznay/employee-directory/static/js/main.e0830073.chunk.js"
+  },
+  {
+    "revision": "dd13a79e7e8c410fb948",
+    "url": "/dbreznay/employee-directory/static/js/runtime-main.cb543d1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dbreznay/employee-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
